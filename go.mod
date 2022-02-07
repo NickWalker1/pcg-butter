7424e6dbb848a0ce76f397d8afd741fef8edf553
Nicholas Walker <nickwalk9@gmail.com>
2022-02-07 16:58:18 +0000

    Basic working

--- go.mod
+++ go.mod
@@ -1,3 +1,19 @@
 module pcg-butter
 
 go 1.17
+
+require (
+	github.com/a-shine/butter v0.0.0-20220207161947-7fb900f930ed
+	github.com/nu7hatch/gouuid v0.0.0-20131221200532-179d4d0c4d8d
+)
+
+require (
+	github.com/jordwest/mock-conn v0.0.0-20180617021051-4896c6bd1641 // indirect
+	github.com/pbnjay/memory v0.0.0-20210728143218-7b4eea64cf58 // indirect
+	gitlab.com/NebulousLabs/fastrand v0.0.0-20181126182046-603482d69e40 // indirect
+	gitlab.com/NebulousLabs/go-upnp v0.0.0-20211002182029-11da932010b6 // indirect
+	golang.org/x/crypto v0.0.0-20210322153248-0c34fe9e7dc2 // indirect
+	golang.org/x/net v0.0.0-20210410081132-afb366fc7cd1 // indirect
+	golang.org/x/sys v0.0.0-20210330210617-4fbd30eecc44 // indirect
+	golang.org/x/text v0.3.6 // indirect
+)
--- main.go
+++ main.go
@@ -1,9 +1,127 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
+	"os"
+	"time"
+
+	"github.com/a-shine/butter"
+	"github.com/a-shine/butter/node"
+	"github.com/a-shine/butter/utils"
+	uuid "github.com/nu7hatch/gouuid"
 )
+
+/* ---- GROUP ---- */
+
+type Group struct {
+	id      uuid.UUID
+	members []utils.SocketAddr
+	dataID  uuid.UUID
+}
+
+func (group *Group) ID() uuid.UUID {
+	return group.id
+}
+
+func (group *Group) DataID() uuid.UUID {
+	return group.dataID
+}
+
+func (group *Group) Members() *[]utils.SocketAddr {
+	return &group.members
+}
+
+/* ---- PEER ---- */
+
+type Peer struct {
+	node   *node.Node
+	groups []Group
+}
+
+func (peer *Peer) GetNode() *node.Node {
+	return peer.node
+}
+
+func (peer *Peer) Groups() *[]Group {
+	return &peer.groups
+}
+
+func (peer *Peer) Spawn() {
+
+}
+
+/* ---- NODE FUNCTIONALITY ---- */
+
+func heartbeatEndpoint(_ *node.Node, payload []byte) []byte {
+	message := string(payload)
+	fmt.Println(message)
+	return []byte("I'm alive too!")
+}
+
+func clientBehaviour(appInterface interface{}) {
+	peer := appInterface.(*Peer)
+	fmt.Println(len(peer.GetNode().KnownHosts()))
+	for {
+		Heartbeat(peer)
+		time.Sleep(time.Second * 10)
+	}
+}
+
+/* For all known hosts ping their heartbeat endpoint to inform them of our status.
+ */
+func Heartbeat(peer *Peer) {
+
+	var recipients = make([]utils.SocketAddr, 0)
+
+	recipients = peer.GetNode().KnownHosts()
+
+	for _, group := range *peer.Groups() {
+		for _, member := range *group.Members() {
+			recipients = append(recipients, member)
+		}
+	}
+
+	fmt.Println(len(recipients))
+	//TODO only send to those we are in groups with.
+
+	for i := 0; i < len(recipients); i++ { // For each known host
+		response, err := utils.Request(recipients[i], []byte("heartbeat/"), []byte("I'm alive. Are you still alive?")) // Uses the utils package (recommended)
+		if err != nil {
+			// If there is an error, log the error BUT DO NOT FAIL - in decentralised application we avoid fatal
+			// errors at all costs as we want to maximise node availability
+			fmt.Println("Unable to send message to", recipients[i])
+		}
+		if bytes.Equal(response, []byte("I'm alive too!")) {
+			fmt.Println(recipients[i], "is okay")
+		}
+		fmt.Println(recipients[i].ToString(), "responded with:", string(response))
+	}
+}
+
+func CreatePeer() *Peer {
+
+	peerNode, err := node.NewNode(0, 2048, clientBehaviour, false)
+	if err != nil {
+		fmt.Println("Fucked it")
+		os.Exit(2)
+	}
+
+	return &Peer{&peerNode, make([]Group, 0)}
+}
 
 func main() {
 	fmt.Println("hello world!")
+
+	peer := CreatePeer()
+
+	fmt.Println("Node is listening at", peer.GetNode().Address())
+
+	// Specifying app level server behaviours - you can specify as many as you like as long as they are not reserved by
+	// other butter packages
+	peer.GetNode().RegisterRoute("heartbeat/", heartbeatEndpoint) // The client behaviour interacts with this route
+
+	// Spawn your node into the butter network
+	butter.Spawn(peer.GetNode(), false, peer) // Blocking
+
 }
